b237336b30a90f95f663f7b611f8e55357837990
SoYoung Kim <sohappy1220@gmail.com>
2021-01-13 20:44:03 +0900

    Updates

---
+++ precache-manifest.5575fa215f3d3a303e83d45b3ca43a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd02f9fca56fef531974ef0cce0d212",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9a1327ef1686b7b5a20a",
+    "url": "/movie_app/static/css/main.3a98a6c7.chunk.css"
+  },
+  {
+    "revision": "fa5e0544dd32d30ca5a4",
+    "url": "/movie_app/static/js/2.9b6f4c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.9b6f4c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1327ef1686b7b5a20a",
+    "url": "/movie_app/static/js/main.9fccd8b4.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
